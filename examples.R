d20ce251cc3473b1b03f45fa5a25d20ad2df7f25
Antoine Logean <antoine.logean@opendata.ch>
2019-03-05 23:46:58 +0100

    change file name of the results

--- examples.R
+++ examples.R
@@ -1,3 +1,3 @@
 #install.packages(c('RCurl','XML','igraph','bitops'),dependencies=TRUE)
 g <- crawl("https://en.wikipedia.org/wiki/Legal_liability", 2)
-save.graph(g, 'mygraph.rds')
+save.graph(g, 'legal_liability_graph')
